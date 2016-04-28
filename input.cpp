da44fc414f859ccd82a725583b804ba3384539ec
karthshen <karth-shen@hotmail.com>
2016-04-27 20:27:34 -0400

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -2,23 +2,25 @@
 #include <string>
 #include <cstring>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 struct realm {
 	string name;
 	int numMagi;
 	vector<int> valueMagi;
+	int num_Connections;
+	vector<realm> connections;
 };
 
 
 vector<realm>  inputRealms()
 {
-	int numRealms; 
+	int numRealms;
 	cin >> numRealms; //input firs tline
-	
+
 	vector <realm> Realms;
-	
+
 	for (int i = 0; i < numRealms; i++)
 	{
 		realm cRealm;
@@ -39,24 +41,33 @@ vector<realm>  inputRealms()
 	return Realms;
 }
 
+void setConnection(realm currentRealm, realm to_be_connected)
+{
+	currentRealm.num_Connections++;
+	currentRealm.connections.push_back(to_be_connected);
+}
+
+
 int main()
 {
 	vector<realm> Realms;
 	Realms = inputRealms();
 	string startRealm, endRealm;
-	cin >> startRealm; cin>> endRealm;
-	/*
+	cin >> startRealm; cin >> endRealm;
+	
 	for (int i = 0; i < 4; i++)
 	{
-		cout << Realms[i].name << endl;
-		cout << Realms[i].numMagi << endl;
-		for (int j = 0; j < Realms[i].numMagi; j++)
-		{
-			cout << Realms[i].valueMagi[j];
-		}
-		cout << endl;
+	cout << Realms[i].name << endl;
+	cout << Realms[i].numMagi << endl;
+	for (int j = 0; j < Realms[i].numMagi; j++)
+	{
+	cout << Realms[i].valueMagi[j];
 	}
-	*/
-	 //Printing result
+	cout << endl;
+	}
+	cout << endl;
+	
+	//Printing result
 	//system("pause");
+	cout << stringDistance("knitting", "knowing") << endl;
 }
